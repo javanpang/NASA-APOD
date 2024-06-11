82d2ca59f993d4232d511c399d212b1048cdada8
javanpang <javanpang@hotmail.com>
2024-06-11 18:29:29 +0100

    Initialise project files

---
+++ reactjs-nasa/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import Sidebar from "./components/Sidebar"
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
